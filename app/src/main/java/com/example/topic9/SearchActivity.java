eabb364c6ea32f378429716b4a0d17ec5c49881c
saroj <sarojpathak430@gmail.com>
2020-01-07 09:02:15 +0545

    Search activity added

--- app/src/main/java/com/example/topic9/SearchActivity.java
+++ app/src/main/java/com/example/topic9/SearchActivity.java
@@ -1,14 +1,130 @@
 package com.example.topic9;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class SearchActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private AutoCompleteTextView etCity;
+    private Button btnSearch;
+    private List<LatitudeLongitude> latitudeLongitudeList;
+    Marker markerName;
+    CameraUpdate center, zoom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(SearchActivity.this);
+
+        etCity = findViewById(R.id.etCity);
+        btnSearch = findViewById(R.id.btnSearch);
+
+        fillArrayListAndSetAdapter();
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (TextUtils.isEmpty(etCity.getText().toString())) {
+                    etCity.setError("Please enter a place name");
+                    return;
+                }
+
+                int position = SearchArrayList(etCity.getText().toString());
+                if (position > -1)
+                    loadMap(position);
+                else
+                    Toast.makeText(SearchActivity.this, "Location Not Found By Name :" + etCity.getText().toString(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
     }
+
+
+    private void fillArrayListAndSetAdapter() {
+        latitudeLongitudeList = new ArrayList<>();
+
+        latitudeLongitudeList.add(new LatitudeLongitude(27.706195, 85.3300396, "Softwarica College"));
+        latitudeLongitudeList.add(new LatitudeLongitude(27.7051477, 85.3287151, "Naagpokhari"));
+        latitudeLongitudeList.add(new LatitudeLongitude(27.704195, 85.3302296, "Narayanhiti"));
+
+        String[] data = new String[latitudeLongitudeList.size()];
+
+        for (int i = 0; i < data.length; i++) {
+            data[i] = latitudeLongitudeList.get(i).getMarker();
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                SearchActivity.this, android.R.layout.simple_list_item_1, data
+        );
+        etCity.setAdapter(adapter);
+        etCity.setThreshold(1);
+
+
+    }
+
+    public int SearchArrayList(String name) {
+        for (int i = 0; i < latitudeLongitudeList.size(); i++) {
+            if (latitudeLongitudeList.get(i).getMarker().contains(name)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        center = CameraUpdateFactory.newLatLng(new LatLng(27.706195, 85.3300396));
+        zoom = CameraUpdateFactory.zoomTo(17);
+
+        mMap.moveCamera(center);
+        mMap.animateCamera(zoom);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+    }
+
+    public void loadMap(int position) {
+        if (markerName != null) {
+            markerName.remove();
+        }
+
+        double latitude = latitudeLongitudeList.get(position).getLat();
+        double longitude = latitudeLongitudeList.get(position).getLog();
+        String marker = latitudeLongitudeList.get(position).getMarker();
+        center = CameraUpdateFactory.newLatLng(new LatLng(latitude, longitude));
+        zoom = CameraUpdateFactory.zoomTo(17);
+        markerName = mMap.addMarker(new MarkerOptions().position(new LatLng(latitude, latitude)).title(marker));
+
+        mMap.moveCamera(center);
+        mMap.animateCamera(zoom);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
+
+    }
+
 }
